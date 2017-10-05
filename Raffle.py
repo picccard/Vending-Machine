421276b0bb6ec669445255f4d62051d5465390aa
Eskil Larsen <eskil.uhlving@gmail.com>
2017-10-05 23:17:00 +0200

    translated raffle class to english

--- Raffle.py
+++ Raffle.py
@@ -1,20 +1,20 @@
 import random
 # https://stackoverflow.com/questions/24108417/simple-way-of-creating-a-2d-array-with-random-numbers-python
 
-class Trekning:
+class Raffle:
     def __init__(self):
-        self.trekningen = [random.randint(1,9) for i in range(5)]
-        self.trekningen.sort()
+        self.draw = [random.randint(1,9) for i in range(5)]
+        self.draw.sort()
 
-    def getTrekningen(self):
-        tempTrekning = []
-        for num in self.trekningen:
-            tempTrekning.append(num)
-        return tempTrekning
+    def getDraw(self):
+        tempDraw = []
+        for num in self.draw:
+            tempDraw.append(num)
+        return tempDraw
 
-    def finnAntallRette(self, tipping):
-        myDict = dict((x,self.trekningen.count(x)) for x in self.trekningen)
-        yourDict = dict((x,tipping.count(x)) for x in tipping)
+    def getMatches(self, guess):
+        myDict = dict((x,self.draw.count(x)) for x in self.draw)
+        yourDict = dict((x,guess.count(x)) for x in guess)
         matches = 0
         for key in myDict.keys():
             if key in yourDict.keys():
